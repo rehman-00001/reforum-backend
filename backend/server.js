2c30d8dd5deb46ab8debfcc564ecc0a53f3d3d6b
Abdul Rehman <abdul.rehman@ymedialabs.com>
2020-08-05 01:36:29 +0530

    dotenv config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-// modules for server
-require('dotenv').config();
+if (app.get('env') == 'development') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const mongoose = require('mongoose');
 
